bdf51f92a514c9aa5678a6113a96e55025299417
G-Maina <python21developer>
2024-03-04 16:07:39 -0800

    Added store and tags apps

---
+++ store/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Sliver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_PENDING = 'p'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_STATUS = [
+        (PAYMENT_PENDING, 'Failed'),
+        (PAYMENT_COMPLETE, 'Complete'),
+        (PAYMENT_FAILED, 'Failed'),
+    ]
+    placed_at =models.DateTimeField(auto_add_now=True) #autopopulate
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_PENDING)
